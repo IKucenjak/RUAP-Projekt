ffbaa9f3521c856652d7e73208d603dfb92a5244
Marko <marko.saric@ferit.hr>
2022-10-27 17:34:27 +0200

    update3

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 
-<<<<<<< HEAD
 namespace RUAP_LV1
-=======
 namespace RUAP_Projekt
->>>>>>> 70f49da3b68b32c05d185b09e75d72e8e1368dd6
 {
     class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Inoslava Kucenjaka 0165081919");
-=======
             Console.WriteLine("Pozdrav iz aplikacije od Inoslava Kucenjaka");
->>>>>>> 70f49da3b68b32c05d185b09e75d72e8e1368dd6
         }
     }
 }
